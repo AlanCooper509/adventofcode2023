c3667078c377ca3c05f28bccd7ccee7dc1cd7062
alancooper509 <alancooper509@hotmail.com>
2023-12-01 20:38:20 -0600

    day 1 (refactored)

--- 01/main.py
+++ 01/main.py
@@ -11,35 +11,50 @@ NUMBER_MAP = {
     "nine": '9'
 }
 
-def main():
-    with open("input.txt", 'r') as file:
-        sum = 0
-        for line in file:
-            first_digit = ''
-            last_digit = ''
-            first_digit = forward_search(line)
-            last_digit = reverse_search(line)
-            sum += int(first_digit + last_digit)
-        print(sum)
+def read_file(file_name):
+    file_contents = ''
+    with open(file_name, 'r') as file:
+        file_contents = file.readlines()
+    return file_contents
 
-def forward_search(input_string):
+def run(file_contents, useWords):
+    sum = 0
+    for line in file_contents:
+        first_digit = forward_search(line, useWords)
+        last_digit = reverse_search(line, useWords)
+        sum += int(first_digit + last_digit)
+    return sum
+
+def forward_search(input_string, useWords):
     if len(input_string) == 0:
         return None
     if input_string[0].isdigit():
         return input_string[0]
-    for key, value in NUMBER_MAP.items():
-        if input_string.startswith(key):
-            return value
-    return forward_search(input_string[1:])
+    if useWords:
+        for key, value in NUMBER_MAP.items():
+            if input_string.startswith(key):
+                return value
+    return forward_search(input_string[1:], useWords)
 
-def reverse_search(input_string):
+def reverse_search(input_string, useWords):
     if len(input_string) == 0:
         return None
     if input_string[-1].isdigit():
         return input_string[-1]
-    for key, value in NUMBER_MAP.items():
-        if input_string.endswith(key):
-            return value
-    return reverse_search(input_string[:-1])
+    if useWords:
+        for key, value in NUMBER_MAP.items():
+            if input_string.endswith(key):
+                return value
+    return reverse_search(input_string[:-1], useWords)
+
+def outputResults(part1answer, part2answer):
+    print(f'Part 1: {part1answer}')
+    print(f'Part 2: {part2answer}')
+
+def main():
+    file_contents = read_file("input.txt")
+    part1answer = run(file_contents, useWords=False)
+    part2answer = run(file_contents, useWords=True)
+    outputResults(part1answer, part2answer)
 
 main()
